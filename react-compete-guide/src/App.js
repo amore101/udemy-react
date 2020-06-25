a8aad43bcb215cc957afb128f25d4227f586c77f
amore <807718119@qq.com>
2020-06-25 00:02:47 -0700

    0

--- react-compete-guide/src/App.js
+++ react-compete-guide/src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// use uppercase to demonstrate difference between html tags
+import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'ruikang', age: '22'},
+      {name: 'jiaxin', age: '22'}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchName = (newName) => {
+    // console.log("Clicked!");
+    this.setState({
+      persons: [
+        {name: newName, age: '22'},
+        {name: 'jiaxin', age: '22'}
+      ]
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1> HI </h1>
+        {/* no '()' */}
+        {/* <button onClick={this.switchName}>Switch name</button> */}
+        <button onClick={() => this.switchName('Rick')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchName.bind(this, 'MaoMao')}>My hobby</Person>
+      </div>
+    );
+  };
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'HI'));
 }
 
 export default App;
+
+
+
+// functional component version
+
+// import React, { useState } from 'react';
+// // use uppercase to demonstrate difference between html tags
+// import Person from './Person/Person';
+// import './App.css';
+
+// const App = props => {
+//   // first argument refers to the current state
+//   // second argument refers to the function to change the state
+//   const [ personState, setPersonState ] = useState ({
+//     persons: [
+//       {name: 'ruikang', age: '22'},
+//       {name: 'jiaxin', age: '22'}
+//     ]
+//   });
+
+//   // use multiple separated state slices
+//   const [ otherState, setOtherState ] = useState('some other value')
+
+//   console.log(personState, otherState);
+
+//   const switchName = () => {
+//     // console.log("Clicked!");
+//     setPersonState({
+//       persons: [
+//         {name: 'Rick', age: '22'},
+//         {name: 'jiaxin', age: '22'}
+//       ],
+//     })
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1> HI </h1>
+//       {/* no '()' */}
+//       <button onClick={switchName}>Switch name</button>
+//       <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+//       <Person name={personState.persons[1].name} age={personState.persons[1].age} >My hobby</Person>
+//     </div>
+//   );
+// }
+
+// export default App;
--- script.md
+++ script.md
@@ -2,3 +2,31 @@
 `npm install create-react-app -g`
 // script version impacts the structure of the project
 `create-react-app react-compete-guide --script-version 1.1.5`
+// clean the App.css and App.js
+
+// create a functional component
+// output dynamic contents
+`I am {Math.floor(Math.random() * 30)} years old`
+
+// transfer props
+props.children refers to contents between tags
+props allow you to pass data from a parent (wrapping) component to a child (embedded) component.
+
+// state is managed inside a component
+state is used to change the component, well, state from within. Changes to state also trigger an UI update.
+
+// handle events: use functions in render
+
+// change state in function using setState
+only change part of the state
+
+// hooks can be used in functional components
+useState is used to manage state
+
+// difference between f & c in changing state
+f - replace; c - merge
+to fix this in f, use multiple separated state slices
+
+// try to use stateless components instead of stateful components
+
+// transfer functions with argument: props and bind
